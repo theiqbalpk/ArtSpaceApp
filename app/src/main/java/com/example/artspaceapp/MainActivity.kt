2c3d3ca6670d7da649c93464ca5385e86315ba54
iqbal Ahmad <theiqbalpk@gmail.com>
2024-02-24 09:32:25 +0500

    Design and UI

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,10 +29,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,13 +72,13 @@ fun ArtSpaceApp() {
     var authorAndYear by remember { mutableStateOf(1) }
 
     val imageResource = when(artImage) {
-        1 -> R.drawable.bourgeois
-        2 -> R.drawable.hockney
-        3 -> R.drawable.koons
-        4 -> R.drawable.imagechristina
-        5 -> R.drawable.lovers
-        6 -> R.drawable.memorysalvordali
-        else -> R.drawable.bourgeois
+        1 -> R.drawable.first
+        2 -> R.drawable.second
+        3 -> R.drawable.third
+        4 -> R.drawable.fourth
+        5 -> R.drawable.fifth
+        6 -> R.drawable.sixth
+        else -> R.drawable.first
     }
 
     val titleString = when(title) {
@@ -77,16 +100,33 @@ fun ArtSpaceApp() {
         6 -> R.string.SalvadorDalí
         else -> R.string.LouiseBourgeois
     }
-
-    Column {
-        ArtworkImage(
-            imageResource = imageResource,
-            contentDescription = titleString
-        )
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(15.dp)
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight(0.65F)
+                .fillMaxWidth(1F)
+                .border(BorderStroke(3.dp, SolidColor(Color.White)), RectangleShape)
+                .shadow(15.dp, RectangleShape)
+                .align(alignment = Alignment.CenterHorizontally),
+            shape = RoundedCornerShape(5.dp),
+            shadowElevation = 10.dp
+        ) {
+            ArtworkImage(
+                imageResource = imageResource,
+                contentDescription = titleString
+            )
+        }
+        Spacer(modifier = Modifier.height(60.dp))
         ArtworkDescription(
             titleString = titleString,
             authorAndYearString = authorAndYearString
         )
+        Spacer(modifier = Modifier.height(20.dp))
         ArtworkButton(
             artImage = artImage,
             title = title,
@@ -117,11 +157,13 @@ fun ArtSpaceApp() {
 @Composable
 fun ArtworkImage(
     @DrawableRes imageResource: Int,
-    @StringRes contentDescription: Int
+    @StringRes contentDescription: Int,
 ) {
     Image(
         painter = painterResource(imageResource),
-        contentDescription = stringResource(contentDescription)
+        contentDescription = stringResource(contentDescription),
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.padding(35.dp)
     )
 }
 
@@ -130,9 +172,38 @@ fun ArtworkDescription(
     @StringRes titleString: Int,
     @StringRes authorAndYearString: Int
 ) {
-    Column {
-        Text(text = stringResource(titleString))
-        Text(text = stringResource(authorAndYearString))
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = Color(0xFFECEBF4)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(20.dp)
+        ) {
+            Text(
+                text = stringResource(titleString),
+                fontSize = 24.sp
+                )
+            
+            val authorAndYearText = stringResource(authorAndYearString)
+
+            val startIndex = authorAndYearText.indexOf('(')
+            val endIndex = authorAndYearText.indexOf(')')
+
+            val author = authorAndYearText.substring(0, startIndex).trim()
+            val year = authorAndYearText.substring(startIndex + 1, endIndex)
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append(author)
+                    }
+                    append(" ($year)")
+                },
+                fontSize = 14.sp
+            )
+        }
     }
 }
 
@@ -144,9 +215,14 @@ fun ArtworkButton(
     previousImage: (Int) -> Unit,
     nextImage: (Int) -> Unit,
     firstImage: (Int) -> Unit,
-    lastImage: (Int) -> Unit
+    lastImage: (Int) -> Unit,
 ) {
-    Row {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         Button(
             onClick = {
             if (artImage != 1 && title != 1 && authorAndYear != 1) {
@@ -154,9 +230,15 @@ fun ArtworkButton(
             } else if ((artImage == 1 && title == 1 && authorAndYear == 1)) {
                 lastImage(artImage)
             }
-        }
+        },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(73, 93, 146)),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.width(150.dp)
         ) {
-            Text(text = "Previous")
+            Text(
+                text = "Previous",
+                color = Color.White
+            )
         }
         Button(
             onClick = {
@@ -165,9 +247,15 @@ fun ArtworkButton(
                 } else if ((artImage == 6 && title == 6 && authorAndYear == 6)) {
                     firstImage(artImage)
                 }
-            }
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(73, 93, 146)),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.width(150.dp)
         ) {
-            Text(text = "Next")
+            Text(
+                text = "Next",
+                color = Color.White
+            )
         }
     }
 }
