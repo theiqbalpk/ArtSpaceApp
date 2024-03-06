da67e8d7904cf0e81f71f38aee82499cdde780f9
iqbal Ahmad <theiqbalpk@gmail.com>
2024-03-06 08:09:41 +0500

    Landscape mode

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.artspaceapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,7 +27,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -36,6 +37,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -66,10 +68,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceApp() {
+    val config = LocalConfiguration.current
+    when (config.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            ArtSpaceAppDisplayLandscape()
+        }
+        else -> {
+            ArtSpaceAppDisplayPortrait()
+        }
+    }
+}
 
-    var artImage by remember { mutableStateOf(1) }
-    var title by remember { mutableStateOf(1) }
-    var authorAndYear by remember { mutableStateOf(1) }
+@Composable
+fun ArtSpaceAppDisplayPortrait() {
+
+    var artImage by remember { mutableIntStateOf(1) }
+    var title by remember { mutableIntStateOf(1) }
+    var authorAndYear by remember { mutableIntStateOf(1) }
 
     val imageResource = when(artImage) {
         1 -> R.drawable.first
@@ -150,6 +165,104 @@ fun ArtSpaceApp() {
             artImage = 6
             title = 6
             authorAndYear = 6
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceAppDisplayLandscape() {
+
+    var artImage by remember { mutableIntStateOf(1) }
+    var title by remember { mutableIntStateOf(1) }
+    var authorAndYear by remember { mutableIntStateOf(1) }
+
+    val imageResource = when (artImage) {
+        1 -> R.drawable.first
+        2 -> R.drawable.second
+        3 -> R.drawable.third
+        4 -> R.drawable.fourth
+        5 -> R.drawable.fifth
+        6 -> R.drawable.sixth
+        else -> R.drawable.first
+    }
+
+    val titleString = when (title) {
+        1 -> R.string.TBourgeois
+        2 -> R.string.THockney
+        3 -> R.string.TKoons
+        4 -> R.string.TWyeth
+        5 -> R.string.TMagritte
+        6 -> R.string.TDalí
+        else -> R.string.TBourgeois
+    }
+
+    val authorAndYearString = when (authorAndYear) {
+        1 -> R.string.LouiseBourgeois
+        2 -> R.string.DavidHockney
+        3 -> R.string.JeffKoons
+        4 -> R.string.AndrewWyeth
+        5 -> R.string.RenéMagritte
+        6 -> R.string.SalvadorDalí
+        else -> R.string.LouiseBourgeois
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(15.dp)
+                .fillMaxWidth(0.35f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(10.dp))
+            Surface(
+                modifier = Modifier
+                    .fillMaxHeight(0.65F)
+                    .fillMaxWidth(1F)
+                    .border(BorderStroke(3.dp, SolidColor(Color.White)), RectangleShape)
+                    .shadow(15.dp, RectangleShape)
+                    .align(alignment = Alignment.CenterHorizontally),
+                shape = RoundedCornerShape(5.dp),
+                shadowElevation = 10.dp
+            ) {
+                ArtworkImage(
+                    imageResource = imageResource,
+                    contentDescription = titleString
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            ArtworkDescription(
+                titleString = titleString,
+                authorAndYearString = authorAndYearString
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            ArtworkButton(
+                artImage = artImage,
+                title = title,
+                authorAndYear = authorAndYear,
+                previousImage = {
+                    artImage -= 1
+                    title -= 1
+                    authorAndYear -= 1
+                },
+                nextImage = {
+                    artImage += 1
+                    title += 1
+                    authorAndYear += 1
+                },
+                firstImage = {
+                    artImage = 1
+                    title = 1
+                    authorAndYear = 1
+                }
+            ) {
+                artImage = 6
+                title = 6
+                authorAndYear = 6
+            }
         }
     }
 }
